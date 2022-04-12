f5902fb76fbf686f5288ae7674e2859fdbedbee9
Davin Harding <davinlharding@gmail.com>
2022-04-12 13:29:33 -0400

    initial commit, working from nuxtjs dynamic routes example, added image, nuxt-img implementation in progress

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center">
+    Welcome to the books app!  Please select a tab from the top navbar to get started.
+  </div>
+</template>
+<script>
+export default {
+  async asyncData() {
+    const mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then((res) => res.json())
+
+    return { mountains }
+  }
+}
+</script>
